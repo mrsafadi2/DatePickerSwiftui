9885306fee794328bbb77d88d3be2e6f7e1b49f2
mrsafadi2 <mrsafadi2@gmail.com>
2022-12-24 09:46:58 +0200

    Initial Commit

---
+++ DatePickerSwiftUI/DatePickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerSwiftUIApp.swift
+//  DatePickerSwiftUI
+//
+//  Created by Mohammed Safadi Macbook Pro on 24/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
